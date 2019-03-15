9a505ae5342c4a26ec578ec9e2d78f12e9c9938d
Valentina Antollini <antolliniv@hotmail.com>
2019-03-15 14:18:39 +0000

    Added Grid

--- walking-robot-grid/Assets/Script/WorldGrid.cs
+++ walking-robot-grid/Assets/Script/WorldGrid.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class WorldGrid : MonoBehaviour
 {
+    public int w = 5;
+    public int h = 5;
+    public Tile tilePrefab;
+    Tile[,] tiles;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tiles = new Tile[w, h];
+        for (var j = 0; j < h; ++j)
+        {
+            for (var i = 0; i < w; ++i)
+            {
+                var tile = Instantiate(tilePrefab);
+                var transform = tile.transform;
+                transform.parent = transform;
+                transform.localPosition = new Vector2(i, j);
+
+                tile.x = i;
+                tile.y = j;
+                tiles[i, j] = tile;
+
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+   
 }
